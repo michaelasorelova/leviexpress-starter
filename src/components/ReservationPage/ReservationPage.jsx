37e0492a8f41f14e45b877ddc5adbe4507b8ef50
Michaela Šorelová <michaelasorelova@gmail.com>
2025-05-17 10:38:21 +0200

    Add HomePage and ReservationPage routes with basic components

---
+++ src/components/ReservationPage/ReservationPage.jsx
@@ -0,0 +1,5 @@
+const ReservationPage = () => (
+  <h2>Vaše e-jízdenka</h2>
+);
+
+export default ReservationPage;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { App } from './components/App';
-import './global.css';
+import { HomePage } from './pages/HomePage';
+import ReservationPage from './components/ReservationPage/ReservationPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'reservation', element: <ReservationPage /> },
+    ],
   },
 ]);
 
-createRoot(document.querySelector('#app')).render(
-  <RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
